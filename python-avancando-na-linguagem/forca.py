dc211320391afbd31c58100fbf481fa4e6e0ce36
Leandro Machado <leandroalvesmachado@gmail.com>
2022-12-07 16:32:17 -0300

    salvando

---
+++ python-avancando-na-linguagem/forca.py
@@ -0,0 +1,25 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "python"
+    enforcou = False
+    acertou = False
+
+    # enquanto não enforcou e não acertou
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando ...")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()

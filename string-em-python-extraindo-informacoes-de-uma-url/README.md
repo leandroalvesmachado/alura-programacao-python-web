e44829f7fe5bd299f7db8cadee2d0cbdac14f91f
Leandro Machado <leandroalvesmachado@gmail.com>
2023-01-04 16:57:49 -0300

    salvando

---
+++ string-em-python-extraindo-informacoes-de-uma-url/README.md
@@ -0,0 +1,11 @@
+# String em Python: extraindo informações de uma URL
+
+## Aula 01 - Introdução e fatiamento de strings
+* [URLs e seus formatos: como as URLs funcionam e o que cada parte de uma URL significa - base e parâmetros.](#)
+* [O operador de fatiamento [a:b], utilizado para obter uma substring desde o índice a até o índice b - 1 da string original. Lembrando que b - 1 pois o segundo argumento do fatiamento é exclusivo.](#)
+* [A string original não é alterada ao ser fatiada devido à sua imutabilidade.](#)
+
+## Aula 02 - Utilizando métodos de strings
+* [](#)
+* [](#)
+* [](#)
--- string-em-python-extraindo-informacoes-de-uma-url/main.py
+++ string-em-python-extraindo-informacoes-de-uma-url/main.py
@@ -1,1 +1,24 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+
+# bytebank.com/cambio?moedaOrigem=real
+print(url)
+
+# string metodo find, retorna a posição do caractere
+indice_interrogacao = url.find('?')
+
+# fatiamento de strings
+
+# url_base = url[0:indice_interrogacao]
+url_base = url[:indice_interrogacao]
+print(url_base) # Vai imprimir bytebank.com/cambio
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros) # Vai imprimir moedaOrigem=real
+
+parametro_busca = "moedaOrigem"
+indice_parametro = url_parametros.find(parametro_busca)
+print(indice_parametro)
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
